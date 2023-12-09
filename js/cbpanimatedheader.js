3aecc34055ba3137773da217845feb2334aa8915
Neringa Geigalaite <neringa@Neringas-MacBook-Pro.local>
2023-12-09 19:00:14 +0200

    Fix

--- js/cbpanimatedheader.js
+++ js/cbpanimatedheader.js
@@ -1,16 +1,25 @@
-
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 40; // Origional 300
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				setTimeout( scrollPage, 250 ); // Origional scrollPage, 250
 			}
 		}, false );
 	}
